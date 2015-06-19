6b4220755013aec9c461d8dcdfb799a00a5412f7
Nathan Prevette <nprevette420@gmail.com>
2015-06-19 00:11:54 -0400

    Update Bliss Configs

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -39,15 +39,28 @@ TARGET_RECOVERY_FSTAB := $(LOCAL_PATH)/rootdir/etc/fstab.qcom
 # inherit from the proprietary version
 -include vendor/motorola/peregrine/BoardConfigVendor.mk
 
+# Block_Build
+Bliss_Build_Block := 1 
+
 # BlissPop Configs
-TARGET_TC_ROM := 4.8
-TARGET_TC_KERNEL := 4.8
+BLISS_WIPE_CACHES := 1
 BLISSIFY := true
 BLISS_O3 := true
-BLISS_STRICT := false
 BLISS_GRAPHITE := true
-BLISS_KRAIT := true
+BLISS_STRICT := false
+BLISS_KRAIT := false
 BLISS_PIPE := true
+FLOOP_NEST_OPTIMIZE := true
+ENABLE_GCCONLY := true
+TARGET_USE_QCOM_BIONIC_OPTIMIZATION := true
+FAST_MATH := true
+ENABLE_MODULAR_O3 := true
+ENABLE_LTO := true
+Link_Time_Optimizations := true
+TARGET_USE_ION_COMPAT := true
+TARGET_USE_KRAIT_PLD_SET := false
+TARGET_TC_ROM := 4.8-linaro
+TARGET_TC_KERNEL := 4.8-linaro
 TARGET_GCC_VERSION_EXP := $(TARGET_TC_ROM)
 TARGET_KERNEL_CUSTOM_TOOLCHAIN := $(TARGET_TC_KERNEL)
 
