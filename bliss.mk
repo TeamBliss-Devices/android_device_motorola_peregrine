bc226034c962070236191b61bd4e64f8d4fde5f8
Nathan Prevette <nprevette420@gmail.com>
2015-07-19 08:17:28 -0700

    Revert "Move audio mod"
    This reverts commit 3b605c76e71a622ea6762de79d2e5a41260123eb.

--- bliss.mk
+++ bliss.mk
@@ -1,10 +1,10 @@
-#Bliss Viper Audio Mod
-BLISS_AUDIO_MOD := viper
-
 $(call inherit-product, device/motorola/peregrine/full_peregrine.mk)
 
 # Inherit some common Bliss stuff.
 $(call inherit-product, vendor/bliss/config/common_full_phone.mk)
+
+#Bliss Viper Audio Mod
+BLISS_AUDIO_MOD := viper
 
 PRODUCT_RELEASE_NAME := peregrine
 PRODUCT_NAME := bliss_peregrine
