afcd86451e239c6ece75b1c25df6610d2e2a8514
Dan Pasanen <dan.pasanen@gmail.com>
2015-08-25 19:39:07 +0200

    peregrine: Specify GMS client id
    Change-Id: I1ddc93474789971d9f20c883ae5921f53451cdc8

--- bliss.mk
+++ bliss.mk
@@ -5,3 +5,5 @@ $(call inherit-product, vendor/bliss/config/common_full_phone.mk)
 
 PRODUCT_RELEASE_NAME := peregrine
 PRODUCT_NAME := bliss_peregrine
+
+PRODUCT_GMS_CLIENTID_BASE := android-motorola
